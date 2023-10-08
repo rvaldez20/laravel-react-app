808eb9f11b1803a8c1b526b59c1bc2a190b4d488
rvaldez20 <rvaldez20@gmail.com>
2023-10-08 15:14:08 -0600

    [ADD] create routes and views(componentes) for: Dashboard | Surveys | Login | Signup

--- react/src/router.jsx
+++ react/src/router.jsx
@@ -1,12 +1,29 @@
-import { createBrowserRouter } from "react-router-dom";
-import App from "./App";
+import { createBrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import Dashboard from './views/Dashboard';
+import Surveys from './views/Surveys';
+import Login from './views/Login';
+import Signup from './views/Signup';
 
 
 const router = createBrowserRouter([
   {
-    path: '/app',
-    element: <App />
-  }
+    path: '/',
+    element: <Dashboard />
+  },
+  {
+    path: '/surveys',
+    element: <Surveys />
+  },
+  {
+    path: '/login',
+    element: <Login />
+  },
+  {
+    path: '/signup',
+    element: <Signup />
+  },
 ])
 
 export default router
