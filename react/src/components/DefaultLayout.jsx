3b2f6ba2042dbd109f60aad09308425a9ffa98c3
rvaldez20 <rvaldez20@gmail.com>
2023-10-08 17:18:25 -0600

    [ADD] config outlet for contente in DefaultLayout for Dashboard and Surveys

--- react/src/components/DefaultLayout.jsx
+++ react/src/components/DefaultLayout.jsx
@@ -1,6 +1,7 @@
 import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+import { Outlet } from 'react-router-dom'
 
 const user = {
   name: 'Tom Cook',
@@ -180,14 +181,7 @@ export default function DefaultLayout() {
           )}
         </Disclosure>
 
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
-          </div>
-        </header>
-        <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">{/* Your content */}</div>
-        </main>
+        <Outlet />
       </div>
     </>
   )
