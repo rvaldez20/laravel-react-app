3e5da18bc1100f46ef73b62bec51107b12c9ba1f
rvaldez20 <rvaldez20@gmail.com>
2023-10-19 15:14:58 -0600

    [ADD] create 3 methods in AuthController: signup, login, logout

---
+++ app/Http/Controllers/AuthCotroller.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use Illuminate\Http\Request;
+
+class AuthCotroller extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+
+    }
+
+    public function login(LoginRequest $request)
+    {
+
+    }
+
+    public function logout(Request $request)
+    {
+
+    }
+}
